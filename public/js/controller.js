6b310acc17db4b4a1d72606113df045be3fb2d5b
riju.nazar <rinzar@gmail.com>
2013-10-29 22:15:29 +0530

    added controller for friends list and user info

--- public/js/controller.js
+++ public/js/controller.js
@@ -3,9 +3,22 @@
 
     var app = angular.module('twitterGalleryApp', []);
     
-    app.controller('friendsListController', function (scope, http) {
-        http.get('/api/friends/').success(function (data) {
-            scope.friends = data;
+    app.controller('friendsList', function ($scope, $http) {
+        
+        $scope.templateURL = "partials/friends.html";
+        $scope.isLoading = true;
+        
+        $http.get('/api/friends/').success(function (data) {
+            $scope.friends = data.users;
+            $scope.nextCursor = data.next_cursor;
+            $scope.isLoading = false;
+        });
+    });
+    
+    
+    app.controller('user', function ($scope, $http) {
+        $http.get('/api/profile/').success(function (user) {
+            $scope.user = user;
         });
     });
     
