0b4b650689a0585b4372a8da2ccf03edd2ca9d7d
riju.nazar <rinzar@gmail.com>
2013-10-31 00:13:08 +0530

    angular router added

--- public/js/controller.js
+++ public/js/controller.js
@@ -3,6 +3,22 @@
 
     var app = angular.module('twitterGalleryApp', []);
     
+    
+    app.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/welcome', {
+            templateUrl: 'partials/welcome.html',
+            controller: 'welcome'
+        }).when('/', {
+            templateUrl: 'partials/main.html',
+            controller: 'user'
+        }).when('/friends/:id', {
+            templateUrl: 'partials/main.html',
+            controller: 'friendsGallery'
+        }).otherwise({
+            redirectTo: '/'
+        });
+    }]);
+
     app.controller('friendsList', function ($scope, $http) {
         
         $scope.templateURL = "partials/friends.html";
@@ -15,11 +31,22 @@
         });
     });
     
+    app.controller('welcome', function ($scope, $http, $location) {
+        $scope.home = '/';
+    });
     
-    app.controller('user', function ($scope, $http) {
+    
+    app.controller('user', function ($scope, $http, $location, $rootScope) {
         $http.get('/api/profile/').success(function (user) {
             $scope.user = user;
+            $rootScope.userId = user.id;
+        }).error(function(data, status, headers, config) {
+            if (status === 401) {
+                $location.path('/welcome');
+            }
         });
     });
+    
+    
     
 })(window.angular);
